34bae0b1142e0b936db1b0dd9a7c1e92434ba6c1
junior <freesoftandskate@gmail.com>
2018-01-28 02:16:19 -0500

    init react

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+// Dependencies
+import React , { Component } from 'react';
+
+// Routes
+import Home from './home/components/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "App" >
+        <Home />
+      </div>
+    );
+  }
+}
+export default App;
